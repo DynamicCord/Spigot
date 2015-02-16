21c0be8f2a12070df496fab5d6b171a8ed05543b
TheFreakLord <freack1208@gmail.com>
2015-02-16 15:17:25 +0100

    First commit.

---
+++ src/me/freack100/dynamicspigot/DynamicSpigot.java
@@ -0,0 +1,13 @@
+/*
+ *
+ *  * Copyright © 2015 Paul Waslowski <freack1208@gmail.com>
+ *  * This work is free. You can redistribute it and/or modify it under the
+ *  * terms of the Do What The Fuck You Want To Public License, Version 2,
+ *  * as published by Sam Hocevar. See the LICENSE file for more details.
+ *
+ */
+
+package me.freack100.dynamicspigot;
+
+public class DynamicSpigot  {
+}
